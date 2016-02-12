99a0481f3705936c058e6c4a1d5b195d7ec9adc8
Daniel López <daniel_lopez@cbm.csic.es>
2016-02-12 16:42:03 +0100

    se muestra el progreso de subida de los fichero utilizando funciones ob_flush() y flush() de PHP

--- www/uploadVideo.php
+++ www/uploadVideo.php
@@ -5,14 +5,15 @@
 <body>
 
 <?php
-	$targetDir=system('echo $PWD') . "/motion/";	
+	$targetDir=exec('echo $PWD') . "/motion/";	
 	$id=htmlspecialchars($_GET["id"]);
-	
 	while ( file_exists(getenv("MOTION_TEMP_DIR") . $id . ".avi") ){
 		sleep(10);
 	}
 	
 	echo '<h5>Transfiriendo imagen...</h5>';
+	ob_flush();
+  flush();
 	echo '<pre>';
 	system(getenv("RCLONE_BIN") . ' copy ' . $targetDir . $id . ".jpg" . " google:SVVPA/imagenes 2>&1", $out1);	
 	echo '</pre>';
@@ -20,17 +21,19 @@
 //	echo '<br><br>';	
 
 	echo '<h5>Transfiriendo video...</h5>';
+	ob_flush();
+  flush();
 	echo '<pre>';
 	system(getenv("RCLONE_BIN") . ' copy ' . $targetDir . $id . ".mp4" . " google:SVVPA/videos 2>&1", $out2);
 	echo '</pre>';
-
 	echo '<br>';	
 	if ($out1==0 && $out2==0){
 		echo '<h3>Google Drive sincronizado correctamente!!</h3>';
 	} else{
 		echo '<h3 style="color:red">¡Ha habido algún problema al sincronizar los archivos. Inténtalo de nuevo más tarde y si sigue fallando contacta con Er Danié!</h3>';
 	}
-	
+	ob_flush();
+  flush();	
 ?>
 
 </body>
