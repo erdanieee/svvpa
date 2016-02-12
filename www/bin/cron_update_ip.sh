51130f02e35895ba46fe6ff1de9a0fe8a49a9243
Daniel López <daniel_lopez@cbm.csic.es>
2016-02-12 17:17:48 +0100

    modificado cron_update para que suba el archivo con la IP a Google Drive

--- www/bin/cron_update_ip.sh
+++ www/bin/cron_update_ip.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+if [[ -z "$CURRENT_IP_FILE" || -z "${DUCKDNS_TOKEN}" ]]
+then
+	echo "Error!! No se ha cargado el archivo fuente con todas las variables!!!"
+	exit 1
+fi
+
 echo "Updating svvpa.duckdns.org"
-echo url="https://www.duckdns.org/update?domains=svvpa&token=b4d602f7-a21f-442d-a8da-a6bff50795f3&ip=" | curl -k -K -
+echo url="https://www.duckdns.org/update?domains=${DUCKDNS_DOMAIN}&token=${DUCKDNS_TOKEN}&ip=" | curl -k -K -
 r1=$?
-
-
 echo
-echo "Saving ip to current_ip.txt"
-wget "http://ipecho.net/plain" -qO current_ip.txt
+
+echo "Saving ip to ${CURRENT_IP_FILE}"
+ip=$(wget "http://ipecho.net/plain" -qO -)
 r2=$?
 
-if [ $r2 ]
+if [[ $r2 -eq 0 ]]
 then
   echo "OK"
 else
@@ -18,10 +23,16 @@ else
 fi
 
 
-if [[ $r1 && $r2 ]]
+echo "Uploading IP file to Google Drive"
+echo "http://${DUCKDNS_DOMAIN}.duckdns.org:${APACHE_PORT}" > ${CURRENT_IP_PATH}${CURRENT_IP_FILE} 
+echo "http://$ip:${APACHE_PORT}" >> ${CURRENT_IP_PATH}${CURRENT_IP_FILE}
+${RCLONE_BIN} copy ${CURRENT_IP_PATH}${CURRENT_IP_FILE} google:SVVPA/ 2>&1
+r3=$?
+
+if [[ $r1 -eq 0 && $r2 -eq 0  && $r3 -eq 0 ]]
 then
   echo
-  echo "Ip actualizada correctamente. Este servidor se puede acceder desde http://svvpa.duckdns.org o en http://$(cat current_ip.txt)"
+  echo "Ip actualizada correctamente. Este servidor se puede acceder desde $(cat ${CURRENT_IP_PATH}${CURRENT_IP_FILE})"
 else
   echo "Hubo errores. Por favor, revisa los parámetros y si los sitios web están funcionando"
   exit 1		
--- www/bin/env.sh
+++ www/bin/env.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-export APACHE_DIR="/var/www/"		#comprobar valores en motion.conf!!!
-
+export APACHE_DIR="/var/www/html/svvpa/www/"		#comprobar valores en motion.conf!!!
+export APACHE_PORT="9999"
+export RCLONE_BIN="/home/dan/software/rclone-v1.27-linux-amd64/rclone"
+export DUCKDNS_DOMAIN="svvpa"
+export DUCKDNS_TOKEN="b4d602f7-a21f-442d-a8da-a6bff50795f3"
+export CURRENT_IP_PATH=${APACHE_DIR}"bin/"
+export CURRENT_IP_FILE="current_ip.txt"
 
