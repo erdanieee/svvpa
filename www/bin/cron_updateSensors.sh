d2f5999ad4df21f28cb50029c36624cb70795cb0
Daniel López <daniel_lopez@cbm.csic.es>
2016-02-12 18:13:59 +0100

    modificado updateSensors para que se suba también a Google Drive

--- www/bin/cron_updateSensors.sh
+++ www/bin/cron_updateSensors.sh
@@ -19,14 +19,6 @@ dht22Temp="$(echo $dht22Val|awk '{print $1}')"
 dht22Hr="$(echo $dht22Val|awk '{print $2}')"
 
 
-echo $cpuTemp
-echo $bmp180Pres
-echo $bmp180Temp
-echo $dht22Val
-echo $dht22Temp
-echo $dht22Hr
-echo
-
 [ "$cpuTemp" ] && cpuTemp="'$cpuTemp'" || cpuTemp="NULL"
 [ "$bmp180Pres" ] && bmp180Pres="'$bmp180Pres'" || bmp180Pres="NULL"
 [ "$bmp180Temp" ] && bmp180Temp="'$bmp180Temp'" || bmp180Temp="NULL"
@@ -43,5 +35,7 @@ echo $dht22Hr
 echo
 
 mysql -u temp -preadtemperature svvpa < <(echo "insert into sensors (CPU_temp, BMP180_temp, BMP180_press, DHT22_temp, DHT22_HR) values ($cpuTemp, $bmp180Temp, $bmp180Pres, $dht22Temp, $dht22Hr)")
+
+./updateGoogleSpreadsheetSensors.py $cpuTemp $bmp180Temp $bmp180Pres $dht22Temp $dht22Hr
 
 #echo "insert into sensors (CPU_temp, BMP180_temp, BMP180_press, DHT22_temp, DHT22_HR) values ($cpuTemp, $bmp180Temp, $bmp180Pres, $dht22Temp, $dht22Hr)"
---
+++ www/bin/updateGoogleSpreadsheetSensors.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import sys
+import json
+import time
+import gspread
+from oauth2client.client import SignedJwtAssertionCredentials
+
+
+
+def main(argv):
+	if len(argv) >= 6:
+		datetime	= time.strftime("%Y/%m/%d %H:%M:%S")
+		cpuTemp		= argv[1]
+		bmp180Temp= argv[2]
+		bmp180Pres= argv[3]
+		dht22Temp	= argv[4]
+		dht22Hr		= argv[5]
+
+		print "Date: " + datetime
+		print "CPU Temp: " + cpuTemp
+		print "BMP180 Temp: " + bmp180Temp
+		print "BMP180 Press: " + bmp180Pres
+		print "DHT22 Temp: "  + dht22Temp
+		print "DHT22 HR: "  + dht22Hr
+		print 
+
+		print "load json and credentials"
+		json_key = json.load(open('svvpa-81b7a04fc4c2.json'))
+		scope = ['https://spreadsheets.google.com/feeds']
+		credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
+
+		print "authorize gspread"
+		gc = gspread.authorize(credentials)
+		print "open sheet"
+		wks = gc.open("test").sheet1
+		print "append row"
+		wks.append_row([datetime, cpuTemp, bmp180Temp, bmp180Pres, dht22Temp, dht22Hr])
+
+		print "DONE!"
+
+	else:
+		print "USAGE " + argv[0] + " <CPU Temp> <BMP180 Temp> <BMP180 Pres> <DHT22 Temp> <DHT22 HR>"
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
