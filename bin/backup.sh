25503f87a2ce393a90ab7c219556e5bd2e155000
Er Danié <erdanieee@gmail.com>
2016-02-17 17:03:59 +0100

    sustituida variable que no existía por una constante

--- bin/backup.sh
+++ bin/backup.sh
@@ -3,5 +3,5 @@
 source CONSTANTS.sh
 
 mkdir ~/backup 2>/dev/null
-tar -zcvf ~/backup/www-`date +%Y%m%d`.tar.gz --exclude=${MOTION_DIR} $dir
+tar -zcvf ~/backup/www-`date +%Y%m%d`.tar.gz --exclude=${MOTION_DIR} ${APACHE_DIR}
 
