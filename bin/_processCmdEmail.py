35953d9b10bcecc03227ac41d170d185ac4d35fa
Er Danié <erdanieee@users.noreply.github.com>
2016-03-04 11:32:55 +0100

    añadida función enviar a google drive

--- bin/_processCmdEmail.py
+++ bin/_processCmdEmail.py
@@ -8,73 +8,90 @@ import gmail_sender as gsender
 import gmail as greader
 
 
-def cmd_help(a):	
+def cmd_help(a=None):	
 	msg1 = gsender.Message(	
 		subject = u"Ayuda CMD_SVVPA",
 		to			= os.environ['EMAIL_ADDR'],
-#		text		= u"Éste es el cuerpo del mensaje en texto plano",
+		#	text		= u"Éste es el cuerpo del mensaje en texto plano",
 		html		= u'''
-						<html>
-							<body>
-								<h3>Ayuda del control remoto de SVVPA mediante emails</h3>
-								<p>SVVPA tiene la capacidad de recibir comandos a través de correo electrónico. Para enviar un comando, manda un <b>email</b> a <a href="mailto:{correo}">{correo}</a> cuyo <b>asunto</b> contenga <b>CMD_SVVPA <i>COMANDO ARG1 ARG2 ARG3 ... ARGn</i></b>, siendo <i>COMANDO</i> el comando que se desea ejecutar y <i>ARGi</i> los argumentos si son requeridos por el comando (Ej: CMD_SVVPA AYUDA). Los comandos disponibles por el momento son:
-								<ul>
-									<li><b>AYUDA</b> - Envía este email con la ayuda de los comandos disponibles. <a href="mailto:{correo}?subject=CMD_SVVPA AYUDA">Ver ejemplo</a></li>	
-									<li><b>GUARDAR_EN_GOOGLE_DRIVE codigoDelEvento</b> - Guarda en google drive la imagen y el vídeo que corresponde al evento con código <i>codigoDelEvento</i>. El código del evento se puede obtener del asunto del email que se envía automáticamente cuando se detecta un movimiento. <a href="mailto:{correo}?subject=CMD_SVVPA GUARDAR_EN_GOOGLE_DRIVE 2016_01_02_15_30_13_12332_123_543_23_5543_12">Ver ejemplo</a></li>
-									<li><b>ESTADO_DEL_SISTEMA</b></li>
-									<li><b>VISTA_EN_DIRECTO</b></li>
-									<li><b>REINICIAR</b></li>
-								</ul>							
-							</body>
-						</html>
-						'''.format(correo=os.environ['GMAIL_ACCOUNT_ALIAS']))
+			<html>
+				<body>
+					<h3>Ayuda del control remoto de SVVPA mediante emails</h3>
+					<p>SVVPA tiene la capacidad de recibir comandos a través de correo electrónico. Para enviar un comando, manda un email a <a href="mailto:{correo}">{correo}</a> cuyo asunto contenga <i>CMD_SVVPA COMANDO ARG1 ARG2 ARG3 ... ARGn</i>, siendo <i>COMANDO</i> el comando que se desea ejecutar y <i>ARGi</i> los argumentos si son requeridos por el comando (Ej: CMD_SVVPA AYUDA). Los comandos disponibles por el momento son:
+					<ul>
+						<li><b>AYUDA</b> - Envía este email con la ayuda de los comandos disponibles. <a href="mailto:{correo}?subject=CMD_SVVPA AYUDA">Ver ejemplo</a></li>	
+						<li><b>GUARDAR_EN_GOOGLE_DRIVE codigoDelEvento</b> - Guarda en google drive la imagen y el vídeo que corresponde al evento con código <i>codigoDelEvento</i>. El código del evento se puede obtener del asunto del email que se envía automáticamente cuando se detecta un movimiento. <a href="mailto:{correo}?subject=CMD_SVVPA GUARDAR_EN_GOOGLE_DRIVE 2016_01_02_15_30_13_12332_123_543_23_5543_12">Ver ejemplo</a></li>
+						<li><b>ESTADO_DEL_SISTEMA</b></li>
+						<li><b>VISTA_EN_DIRECTO</b></li>
+						<li><b>REINICIAR</b></li>
+					</ul>							
+				</body>
+			</html>
+	'''.format(correo=os.environ['GMAIL_ACCOUNT_ALIAS']))
 
 	s = gsender.GMail(os.environ['SMPT_USER'], os.environ['SMPT_PASS'])
 	s.send(msg1)
 	s.close()
-	return a
 
-def cmd_saveFile(a):
-	imageFile = a + "." + os.environ['MOTION_IMAGE_EXT']
-	videoFile = a + "." + os.environ['MOTION_VIDEO_EXT']
-	imageLogFile = "/tmp/" + a + "_IMAGEN.log"
-	videoLogFile = "/tmp/" + a + "_VIDEO.log"
-	imageCmd = os.environ['RCLONE_BIN'] + " --config " + os.environ['RCLONE_CONFIG'] + " copy " + os.environ['MOTION_DIR'] + imageFile + " google:SVVPA/imagenes 2>&1 > " + imageLogFile
-	videoCmd = os.environ['RCLONE_BIN'] + " --config " + os.environ['RCLONE_CONFIG'] + " copy " + os.environ['MOTION_DIR'] + videoFile + " google:SVVPA/imagenes 2>&1 > " + videoLogFile
 
-	cmdTEMPORAL = os.environ['RCLONE_BIN'] + " --config " + os.environ['RCLONE_CONFIG'] + " copy /home/dlopez/temp/x.py google:SVVPA/imagenes 2>&1 > " + videoLogFile
+def cmd_saveFile(eventId):
+	errorMsg=""
+	imageFile = os.environ['MOTION_DIR'] +  eventId + "." + os.environ['MOTION_IMAGE_EXT']
+	videoFile = os.environ['MOTION_DIR'] +  eventId + "." + os.environ['MOTION_VIDEO_EXT']
+	imageLogFile = "/tmp/" +  eventId + "_IMAGEN.log"
+	videoLogFile = "/tmp/" +  eventId + "_VIDEO.log"
+	imageCmd = os.environ['RCLONE_BIN'] + " --config " + os.environ['RCLONE_CONFIG'] + " copy " + imageFile + " google:SVVPA/imagenes 2>&1 |tee " + imageLogFile
+	videoCmd = os.environ['RCLONE_BIN'] + " --config " + os.environ['RCLONE_CONFIG'] + " copy " + videoFile + " google:SVVPA/imagenes 2>&1 |tee " + videoLogFile
 
-	imageStatus = proc.call(cmdTEMPORAL)
-	videoStatus = proc.call(cmdTEMPORAL)
+	if not os.path.isfile(imageFile):
+		raise Exception('{0}: No se encuentra la imagen del evento! Comprueba que has escrito correctamente el identificador del evento'.format(imageFile))	
+	if not os.path.isfile(videoFile):
+		raise Exception('No se encuentra el vídeo del evento! Es posible que aún se esté procesando. Por favor, inténtalo de nuevo más tarde')
+#	if len(eventId.split("_")) < 12:
+#		raise Exception('Error en el identificador del evento "{0}". Recuerda que el identificador son 12 números separados por guiones bajos'.format(eventId))
 
-	if imageStatus and videoStatus:
-		asunto="Transferencia correcta (" + a + ")"
-		texto="La imagen y el vídeo se han subido correctamente"
+	try:
+		print "Enviando imagen"
+		imageCmdResult = proc.call(imageCmd, shell=True)
+	except Exception as e:
+		errorMsg+='Error al enviar la imagen a google drive.\n' + str(e) + '\n'
+		#raise type(e)('Error al enviar el archivo a google drive.\n' + str(e) + '\n' + imageCmd)
+
+	try:
+		print "Enviando vídeo"
+		videoCmdResult = proc.call(videoCmd, shell=True)
+	except Exception as e:
+		errorMsg+='Error al enviar el vídeo a google drive.\n' + str(e) + '\n'
+	
+	if imageCmdResult or videoCmdResult or errorMsg:
+		print "Transferencia con errores"
+		asunto=u"Transferencia con ERRORES (" +  eventId + ")"
+		texto=u"Ha habido errores al subir los arhivos a google drive. Adjunto se envían los logs de las transferencias.\n" + errorMsg
 	else:
-		asunto="Transferencia con ERRORES (" + a + ")"
-		texto="Ha habido errores al subir los arhivos a google drive. Adjunto se envían los logs de las transferencias."
-
-	s = gsender.GMail(os.environ['SMPT_USER'], os.environ['SMPT_PASS'])
-	msg1 = gsender.Message(	subject 		= "asunto sin sunto, A!",
-													to 		 			= os.environ['EMAIL_ADDR'],
-													text 				= u"Éste es el cuerpo del mensaje en texto plano",
-													attachments = [imageLogFile, videoFile])
-	s.send(msg1)
+		print "Transferencia correcta"
+		asunto=u"Transferencia correcta (" +  eventId + ")"
+		texto=u"La imagen y el vídeo se han subido correctamente a google drive"
+	
+	s 	 = gsender.GMail(os.environ['SMPT_USER'], os.environ['SMPT_PASS'])
+	msg = gsender.Message(	subject 		= asunto,
+									to 		 	= os.environ['EMAIL_ADDR'],
+									text 			= texto,
+									attachments	= [imageLogFile, videoLogFile])
+	s.send(msg)
 	s.close()
+
+def cmd_status(args):
 	return a
 
-def cmd_status(a):
+def cmd_reboot(args):
 	return a
 
-def cmd_reboot(a):
-	return a
-
-def cmd_lifeCam(a):
+def cmd_lifeCam(args):
 	return a
 
 
 def main(args):
-	re_subject = re.compile('CMD_SVVPA (?P<cmd>\w+)(?P<args>.*)')
+	re_subject = re.compile('CMD_SVVPA (?P<cmd>\w+) (?P<args>.*)')
 	CMD_SVVPA={
 		'AYUDA' 									: cmd_help,
 		'GUARDAR_EN_GOOGLE_DRIVE'	: cmd_saveFile,
@@ -88,7 +105,7 @@ def main(args):
 	
 	for e in emails:
 		if e.has_label('procesando'):
-			#comprobar que no lleva mucho tiempo procesándose...
+			#TODO: comprobar que no lleva mucho tiempo procesándose?
 			print "El email ya se está procesado"
 			continue
 	
@@ -98,19 +115,19 @@ def main(args):
 			if r and CMD_SVVPA.has_key(r.group('cmd')):
 				print "Comando recibido: " + r.group('cmd') + " " + r.group('args')
 				try:
-					print "procesando"
-					e.add_label('procesando')
+					print "Procesando email"
+					#e.add_label('procesando')
 					CMD_SVVPA[r.group('cmd')](r.group('args'))
 					print "OK"
-					e.add_label('OK')			
+					#e.add_label('OK')			
 				
 				except Exception, ex:
-					print "Ha ocurrido algún error al procesar el comando"					
+					print "Ha ocurrido el siguiente error al procesar el comando:"					
 					print ex
-					e.add_label('CMD_ERROR')
-					#enviar email de error!
+					#e.add_label('CMD_ERROR')
+					#TODO: enviar email de error?
 		
-				e.read()
+				#e.read()
 					
 			else:
 				e.add_label('CMD_ERROR')
