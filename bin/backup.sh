98c18587742ef6de5cce23e30a680f3ca93f6389
Er Danié <erdanieee@gmail.com>
2016-03-09 19:26:30 +0100

    corregido error sintáctico

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source $(dirname $0)/CONSTANTS.sh
-exec &> >(tee -a $LOG_FILE})
+exec &> >(tee -a {$LOG_FILE})
 
 mkdir ~/backup 2>/dev/null
 f=~/backup/www-`date +%Y%m%d`.tar.gz
--- bin/cron_processCmdEmail.sh
+++ bin/cron_processCmdEmail.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source $(dirname $0)/CONSTANTS.sh
-exec &> >(tee -a $LOG_FILE})
+exec &> >(tee -a {$LOG_FILE})
 
 echo "[$(date)] $0: Procensando comando email"
 python _processCmdEmail.py 
