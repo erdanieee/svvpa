900da06e4f08cfcf6f03e5864c13ffa3da60683a
Er Danié <erdanieee@gmail.com>
2016-03-09 14:01:48 +0100

    Estandarización en el envío de emails usando librería python gmail

--- bin/_sendEmailImage.py
+++ bin/_sendEmailImage.py
@@ -3,13 +3,9 @@
 
 import sys
 import os
-import smtplib
 import time
-import urllib
-from email.mime.text import MIMEText
-from email.MIMEImage import MIMEImage
-from email.mime.multipart import MIMEMultipart
 import datetime as dat
+import gmail_sender as gsender
 
 
 html = """\
@@ -18,7 +14,7 @@ html = """\
 	<body>
 		<h3>Movimiento detectado</h3>
 		<p>S.V.V.P.A. 2.0 ha detectado un nuevo movimiento en E.C. {datetime}. Adjunto a este mensaje se incluye el fotograma más representativo.</p>
-		<p>Si el vídeo es interesante y deseas guardarlo en Google Drive, haz <a href="http://{dom}.duckdns.org:{port}/uploadVideo.php?id={id}">click aquí</a>. Al abrir esa página en un navegador, SVVPA inicia la subida del vídeo y muestra una web con el progreso. <b>No</b> es necesario dejar la página abierta hasta que termine; puedes cerrarla, cerrar el navegador o incluso apagar tu máquina sin que interfiera el proceso. Recuerda que, en función del tamaño del vídeo, este proceso puede tardar varios minutos.</p>
+		<p>Si el vídeo es interesante y deseas guardarlo en Google Drive, haz <a href="http://{dom}.duckdns.org:{port}/uploadVideo.php?id={id}">click aquí</a>. Al abrir esa página en un navegador, SVVPA iniciará la subida del vídeo y mostrará una web con el progreso. <b>No</b> es necesario dejar la página abierta hasta que termine; puedes cerrarla, cerrar el navegador o incluso apagar tu máquina sin que interfiera el proceso. Recuerda que, en función del tamaño del vídeo, este proceso puede tardar varios minutos.</p>
 	 	<p>Puedes ver las capturas guardadas anteriormente en <a href="https://drive.google.com/folderview?id=0Bwse_WnehFNKT2I3N005YmlYMms&usp=sharing">este enlace</a>.</p>
 		<p>Para acceder <b>de forma remota</b> a SVVPA visita <a href="http://{dom}.duckdns.org:{port}">http://{dom}.duckdns.org:{port}</a> o <a href="http://{ip}:{port}">http://{ip}:{port}</a>.	El consumo de datos hasta ahora ha sido de {datos}Mb de los {datosMensuales}Mb mensuales que incluye la tarifa.		 
 	</body>
@@ -30,8 +26,8 @@ html = """\
 		<h3>Movimiento detectado</h3>
 		<p>S.V.V.P.A. 2.0 ha detectado un nuevo movimiento en E.C. {datetime}. Adjunto a este mensaje se incluye el fotograma más representativo.</p>
 		<p>Si el vídeo es interesante y deseas guardarlo en Google Drive, haz <a href="mailto:{email}?subject=CMD_SVVPA GUARDAR_EN_GOOGLE_DRIVE {id}">click aquí</a> para enviar enviar un email con el comando correspondiente. Recuerda que, en función del tamaño del vídeo, el este proceso puede tardar varios minutos.</p>
-	 	<p>Puedes ver las capturas guardadas anteriormente en <a href="https://drive.google.com/folderview?id=0Bwse_WnehFNKT2I3N005YmlYMms&usp=sharing">este enlace</a>.</p>
-		<p>El consumo de datos hasta ahora ha sido de {datos}Mb de los {datosMensuales}Mb mensuales que incluye la tarifa.		 
+	 	<p>Recuerda que puedes ver las capturas guardadas anteriormente en <a href="https://drive.google.com/folderview?id=0Bwse_WnehFNKT2I3N005YmlYMms&usp=sharing">este enlace</a>.</p>
+		<p>El consumo de datos hasta el momento ha sido de {datos}Mb de los {datosMensuales}Mb mensuales que incluye la tarifa.		 
 	</body>
 </html>
 """
@@ -45,44 +41,28 @@ def main(argv):
 			datetime = ""
 			
 			if len(tk)>=12:
-				datetime = "el " + tk[0] +"/"+ tk[1] +"/"+ tk[2] +" a las "+ tk[3] +":"+ tk[4] +":"+ tk[5]
+				datetime = "el " + tk[0] +"/"+ tk[1] +"/"+ tk[2] +
+										" a las "+ tk[3] +":"+ tk[4] +":"+ tk[5]
 			else:
-				datetime = "el " + time.strftime("%Y/%m/%d") + " aproximadamente a las " + time.strftime("%H:%M:%S")			
+				datetime = "el " + time.strftime("%Y/%m/%d") + 
+										" aproximadamente a las " + time.strftime("%H:%M:%S")			
 
-			
-
-			# Construct email
-			msg = MIMEMultipart()
-			msg['To'] = os.environ['EMAIL_ADDR']
-			msg['From'] = os.environ['EMAIL_FROM']
-			msg['Subject'] = 'Movimiento detectado (Código:' + os.path.basename(image) + ')
-			msg.preamble = 'Movimiento detectado (Código:' + os.path.basename(image) + ')
-
-			# Attach html	
-			msg.attach(MIMEText(html.format(ip=get_ip(), 
-																			datetime=datetime, 
-																			dom=os.environ['DUCKDNS_DOMAIN'], 
-																			port=os.environ['APACHE_PORT'], 
-																			id=os.path.basename(image).split(".")[0], 
-																			datos=get_datos(), 
-																			datosMensuales=os.environ['DATOS_MENSUALES']
-																			email=os.environ['GMAIL_ACCOUNT_ALIAS'])
-													, 'html', 'utf-8'))
-
-			#attach image
-			fp=open(image,'rb')
-			msg.attach(MIMEImage(fp.read()))
-			fp.close()
-
-			#send email
-			server = smtplib.SMTP(os.environ['SMPT_SERVER'],os.environ['SMPT_PORT']) #port 465 or 587
-			server.ehlo()
-			server.starttls()
-			server.ehlo()
-			server.login(os.environ['SMPT_USER'],os.environ['SMPT_PASS'])
-			server.sendmail(os.environ['EMAIL_FROM'], os.environ['EMAIL_ADDR'], msg.as_string())
-			server.close()
-
+			msg = gsender.Message(	
+				subject 		= u"Movimiento detectado (Código: {})".format(os.path.basename(image)),
+				to 		 			= os.environ['EMAIL_ADDR'],
+				sender			= os.environ['GMAIL_ACCOUNT_ALIAS'],
+				html 				= html.format(
+					ip=get_ip(), 
+					datetime=datetime, 
+					dom=os.environ['DUCKDNS_DOMAIN'], 
+					port=os.environ['APACHE_PORT'], 
+					id=os.path.basename(image).split(".")[0], 
+					datos=get_datos(), 
+					datosMensuales=os.environ['DATOS_MENSUALES']
+					email=os.environ['GMAIL_ACCOUNT_ALIAS']),
+				attachments	= [image])
+			s.send(msg)
+			s.close()
 
 
 		else:
