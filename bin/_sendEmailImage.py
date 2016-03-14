8cd260dd28a59815a70535d9bcc6fb24bd240620
Er Danié <erdanieee@gmail.com>
2016-03-14 16:30:13 +0100

    Errores menores

--- bin/_sendEmailImage.py
+++ bin/_sendEmailImage.py
@@ -20,13 +20,13 @@ html = u"""\
 		<p>Para acceder <b>de forma remota</b> a SVVPA visita <a href="http://{dom}.duckdns.org:{port}">http://{dom}.duckdns.org:{port}</a> o <a href="http://{ip}:{port}">http://{ip}:{port}</a>.	El consumo de datos hasta ahora ha sido de {datos}Mb de los {datosMensuales}Mb mensuales que incluye la tarifa.		 
 	</body>
 </html>
-			""" if os.environ['REMOTE_ACCESS'] else u"""\
+			""" if bool(int(os.environ['REMOTE_ACCESS'])) else u"""\
 <html>
 	<head></head>
 	<body>
 		<h3>Movimiento detectado</h3>
 		<p>S.V.V.P.A. 2.0 ha detectado un nuevo movimiento en E.C. {datetime}. Adjunto a este mensaje se incluye el fotograma más representativo.</p>
-		<p>Si el vídeo es interesante y deseas guardarlo en Google Drive, haz <a href="mailto:{email}?subject=CMD_SVVPA GUARDAR_EN_GOOGLE_DRIVE {id}">click aquí</a> para enviar enviar un email con el comando correspondiente. Recuerda que, en función del tamaño del vídeo, el este proceso puede tardar varios minutos.</p>
+		<p>Si el vídeo es interesante y deseas guardarlo en Google Drive, haz <a href="mailto:{email}?subject=CMD_SVVPA GUARDAR_EN_GOOGLE_DRIVE {id}">click aquí</a> para enviar enviar un email con el comando correspondiente. Recuerda que, en función del tamaño del vídeo, este proceso puede tardar varios minutos.</p>
 	 	<p>Recuerda que puedes ver las capturas guardadas anteriormente en <a href="https://drive.google.com/folderview?id=0Bwse_WnehFNKT2I3N005YmlYMms&usp=sharing">este enlace</a>.</p>
 		<p>El consumo de datos hasta el momento ha sido de {datos}Mb de los {datosMensuales}Mb mensuales que incluye la tarifa.		 
 	</body>
@@ -38,7 +38,8 @@ def main(argv):
 		print "[{}] {}: Enviando email con la captura".format(dat.datetime.now(), __file__)
 		if os.path.exists(sys.argv[1]):
 			image 	 = sys.argv[1]
-			tk	  	 = os.path.basename(image).split("_")
+			id = os.path.basename(image).split(".")[0].strip()
+			tk = id.split("_")
 			datetime = ""
 			
 			if len(tk)>=12:
@@ -47,7 +48,7 @@ def main(argv):
 				datetime = "el " + time.strftime("%Y/%m/%d") + " aproximadamente a las " + time.strftime("%H:%M:%S")			
 			s   = gsender.GMail(os.environ['SMPT_USER'], os.environ['SMPT_PASS'])
 			msg = gsender.Message(	
-				subject 		= u"Movimiento detectado (Código: {})".format(os.path.basename(image)),
+				subject 		= u"Movimiento detectado ({})".format(id),
 				to 		 			= os.environ['EMAIL_ADDR'],
 				sender			= os.environ['GMAIL_ACCOUNT_ALIAS'],
 				html 				= html.format(
@@ -55,7 +56,7 @@ def main(argv):
 					datetime=datetime, 
 					dom=os.environ['DUCKDNS_DOMAIN'], 
 					port=os.environ['APACHE_PORT'], 
-					id=os.path.basename(image).split(".")[0], 
+					id=id, 
 					datos=get_datos(), 
 					datosMensuales=os.environ['DATOS_MENSUALES'],
 					email=os.environ['GMAIL_ACCOUNT_ALIAS']),
