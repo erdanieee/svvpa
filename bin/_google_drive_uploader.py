d29cac853c75c91d89e5f67bc12d13d9d982b05a
Er Danié <erdanieee@users.noreply.github.com>
2016-05-10 16:56:03 +0200

    v1.0

--- bin/_google_drive_uploader.py
+++ bin/_google_drive_uploader.py
@@ -7,6 +7,8 @@ from apiclient.http import MediaFileUpload
 import oauth2client
 import json
 import time
+import datetime
+import random
 
 
 
@@ -15,47 +17,58 @@ import time
 SCOPES = 'https://www.googleapis.com/auth/drive'
 CLIENT_SECRET_FILE = os.environ['CONFIG_DIR'] + 'google_drive_client_secret.json'
 APPLICATION_NAME = 'SVVPA'
-
-
         
         
-def get_credentials():
-    """Gets valid user credentials from storage.
-
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
-    """    
+def get_credentials():    
     credential_path = os.path.join(os.environ['CONFIG_DIR'], 'google-drive-credentials.json')
     store           = oauth2client.file.Storage(credential_path)
     credentials     = store.get()
     if not credentials or credentials.invalid:
         flow            = oauth2client.client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME        
-        credentials     = oauth2client.tools.run(flow, store)
+        credentials     = oauth2client.tools.run_flow(flow, store, oauth2client.tools.argparser.parse_args(args=[]))
         print u"[{}] {}: Guardando credenciales en {}".format(datetime.datetime.now(), __file__, credential_path)
     return credentials
         
         
    
-def uploadFile(file, mimeType):
+def uploadImage(file):
     try:
         credentials     = get_credentials()
         http            = credentials.authorize(httplib2.Http())
         service         = discovery.build('drive', 'v3', http=http)
-        file_metadata   = { 'name' : os.path.basename(file)}
-        media           = MediaFileUpload(file,  mimetype=mimeType)#, resumable=True)
+        file_metadata   = { 'name' : os.path.basename(file), 'parents' : ["0Bwse_WnehFNKWERoYS1wYnMwVVk"]}
+        media           = MediaFileUpload(file,  mimetype='image/jpg')#, resumable=True)
         data            = service.files().create(body = file_metadata,        #TODO: CHECK: en principio solo hace falta la URL
-                                                 media_body = media, 
-                                                 fields = ('webContentLink')).execute()
+                                                 media_body = media).execute()#, 
+                                                 #fields = ('id', 'webViewLink')).execute()
         #print ('File ID: %s' % data.get('id'))
         return data
     
     except Exception as e:
         print u"[{}] {}: ERROR! Hubo un error inesperado al subir el archivo a google drive:\n{}".format(datetime.datetime.now(), __file__, repr(e))
     
+    
+def uploadVideo(file):
+    try:
+        credentials     = get_credentials()
+        http            = credentials.authorize(httplib2.Http())
+        service         = discovery.build('drive', 'v3', http=http)
+        file_metadata   = { 'name' : 'SVVPA/videos' + os.path.basename(file)}
+        media           = MediaFileUpload(file,  mimetype='video/mp4', resumable=True)
+        request         = service.files().create(body = file_metadata,
+                                                 media_body = media, 
+                                                 fields = ('id'))
+                                                 
+        data = None
+        while data is None:
+            status, data = request.next_chunk()                                               
+        #print ('File ID: %s' % data.get('id'))
+        return data
+    
+    except Exception as e:
+        print u"[{}] {}: ERROR! Hubo un error inesperado al subir el archivo a google drive:\n{}".format(datetime.datetime.now(), __file__, repr(e))
+
 
 
 
@@ -83,22 +96,44 @@ def run_query(self, query=''):
          
          
 if __name__ == "__main__":
-    file = sys.argv[1]
+    #TEMPORAL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    #file = '/home/dlopez/Downloads/2016_04_04_15_11_40_8745_14_52_234_566_305_1.jpg' #sys.argv[1]
+    file = '/home/dlopez/temp/video.mp4' #sys.argv[1]
     
-    mimeType = 'image/jpg' if file.endswith('jpg') else 'video/mp4'
-    id       = os.path.basename(file).split(".")[0].strip() 
-    
-    for i in range(20):
-        data = uploadFile(sys.argv[1], mimeType)
+    id   = os.path.basename(file).split(".")[0].strip()
         
-        if data:
-            run_query('insert into security(weblink) values({}) where id like \'{}\')
-            
+    for i in range(20):
+        if file.endswith('.jpg'):
+            data = uploadImage(file)
             
         else:
+            data = uploadVideo(file)
+        
+        if data:
+            break
+        else:
             time.sleep(random.randint(20,60))
+    
+    #TODO: usar service.get(id) para obtener todos los datos del archivo y meter los interesantes en mysql
+        
+    if file.endswith('.jpg'):
+        query = 'update images set uid={}, link={}, width={}, height={} where id like \'{}\''.format(data['id'],
+                                                                                                   data['webContentLink'],
+                                                                                                   data['imageMediaMetadata.width'],
+                                                                                                   data['imageMediaMetadata.height'],
+                                                                                                   id )
+        
+    else:
+        query = 'update videos set uid={}, link={}, width={}, height={} where id like \'{}\''.format(data['id'],
+                                                                                                   data['webContentLink'],
+                                                                                                   data['videoMediaMetadata.width'],
+                                                                                                   data['videoMediaMetadata.height'],
+                                                                                                   id )
+        
+    run_query(query)
+        
 
 
      
         
-      
+        
