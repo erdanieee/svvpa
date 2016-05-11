ceff3f8609ffb418480b2960aa18a48e4fd992f8
Er Danié <erdanieee@users.noreply.github.com>
2016-05-11 12:49:16 +0200

    añadidas tablas images y videos

--- config/svvpa.sql
+++ config/svvpa.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `svvpa` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `svvpa`;
--- MySQL dump 10.13  Distrib 5.5.46, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.49, for debian-linux-gnu (x86_64)
 --
--- Host: 127.0.0.1    Database: svvpa
+-- Host: localhost    Database: svvpa
 -- ------------------------------------------------------
--- Server version	5.5.46-0ubuntu0.14.04.2
+-- Server version	5.5.49-0ubuntu0.14.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +14,33 @@ USE `svvpa`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `svvpa`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `svvpa` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `svvpa`;
+
+--
+-- Table structure for table `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `images` (
+  `id` varchar(80) NOT NULL,
+  `uid` varchar(45) DEFAULT NULL,
+  `link` varchar(80) DEFAULT NULL,
+  `width` smallint(2) DEFAULT NULL,
+  `height` smallint(2) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `uid_UNIQUE` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `sensors`
@@ -36,8 +61,25 @@ CREATE TABLE `sensors` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping routines for database 'svvpa'
+-- Table structure for table `videos`
 --
+
+DROP TABLE IF EXISTS `videos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `videos` (
+  `id` varchar(80) NOT NULL,
+  `uid` varchar(45) DEFAULT NULL,
+  `link` varchar(80) DEFAULT NULL,
+  `width` smallint(2) DEFAULT NULL,
+  `height` smallint(6) DEFAULT NULL,
+  `size` int(11) DEFAULT NULL,
+  `duration` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `uid_UNIQUE` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -48,4 +90,4 @@ CREATE TABLE `sensors` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-02-15 18:25:44
+-- Dump completed on 2016-05-11 12:48:25
