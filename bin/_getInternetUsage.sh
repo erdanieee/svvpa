bdb43918c4216d5cebcfc2cb8c2d97ea6fa44dbc
Er Danié <erdanieee@gmail.com>
2016-05-18 12:14:08 +0200

    Imprime Mebibytes (2^20 bytes) transferidos por la interfaz en el mes

--- bin/_getInternetUsage.sh
+++ bin/_getInternetUsage.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#Imprime los Mb gastados en el mes para la interfaz de red
+#Imprime los Mebibytes (2^20 bytes) gastados en el mes para la interfaz de red
 
 
 source $(dirname $0)/CONSTANTS.sh
 #touch ${LOG_FILE}; exec &> >(tee -a ${LOG_FILE})
 
 ${VNSTAT_BIN} -u -i ${VNSTAT_INTERFACE} 2>&1 > /dev/null
-${VNSTAT_BIN} --dumpdb -i ${VNSTAT_INTERFACE}|egrep 'm;0'|awk -F";" '{print $6+$7}'
+${VNSTAT_BIN} --dumpdb -i ${VNSTAT_INTERFACE}|egrep 'm;0'|awk -F";" '{print $4+$5}'
 
